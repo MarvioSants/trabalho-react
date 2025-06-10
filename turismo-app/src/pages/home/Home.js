039b490baa4566127702b7717e4d8d17296208ee
Marcio Santos <marciosanthos97@gmail.com>
2025-06-10 16:21:01 -0300

    Só uma alteração no h1 de home

--- turismo-app/src/pages/home/Home.js
+++ turismo-app/src/pages/home/Home.js
@@ -5,7 +5,7 @@ import styles from "./Home.module.css"; // Certifique-se de criar este arquivo C
 const Home = () => {
   return (
     <header className="home-container">
-      <h1>Turismo Sustentável/ O lado que você não conhece</h1>
+      <h1>O TURÍSMO QUE VOCÊ NÃO CONHECE</h1>
       <nav>
         <Link to="/destinos">Destinos</Link>
         <Link to="/sabermais">Saber Mais</Link>
@@ -13,7 +13,6 @@ const Home = () => {
         <Link to="/login">Login</Link>
       </nav>
     </header>
-    
   );
 };
 
